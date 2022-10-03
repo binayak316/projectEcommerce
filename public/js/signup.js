e4cdd7cc847d35f9bddf3664f008922180e28111
Samir Karki <karkisamir99@gmail.com>
2022-10-03 21:04:50 +0545

    Ready for authentication part

---
+++ public/js/signup.js
@@ -0,0 +1,34 @@
+// New Password show/hide eye button
+function new_password_show_hide() {
+    var x = document.getElementById("password");
+    var show_eye = document.getElementById("show_eye");
+    var hide_eye = document.getElementById("hide_eye");
+    hide_eye.classList.remove("d-none");
+    if (x.type === "password") {
+        x.type = "text";
+        show_eye.style.display = "none";
+        hide_eye.style.display = "block";
+    } else {
+        x.type = "password";
+        show_eye.style.display = "block";
+        hide_eye.style.display = "none";
+    }
+}
+
+
+// Confirm Password show/hide eye button
+function confirm_password_show_hide() {
+    var x = document.getElementById("confirmPassword");
+    var show_eye = document.getElementById("confirm_show_eye");
+    var hide_eye = document.getElementById("confirm_hide_eye");
+    hide_eye.classList.remove("d-none");
+    if (x.type === "password") {
+        x.type = "text";
+        show_eye.style.display = "none";
+        hide_eye.style.display = "block";
+    } else {
+        x.type = "password";
+        show_eye.style.display = "block";
+        hide_eye.style.display = "none";
+    }
+}
